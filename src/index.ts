f65be07722daef223457935045117eea9136219d
Wemerson Jesus <wejesuss@gmail.com>
2020-07-08 07:59:06 -0300

    Remove environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
-const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.get('/', (req, res) => {
     return res.send('hello world');
 });
 
-app.listen(port);
+app.listen(3000);
