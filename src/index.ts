a1ee399b0e83b2a23bba96ee549eff4b46927d32
Wemerson Jesus <wejesuss@gmail.com>
2020-07-15 17:57:13 -0300

    Search route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import express from 'express';
 
-import db from '../db.json';
+import api from './config/api';
+import { polish } from './utils/util';
+import { Search } from './@types/index';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json(db);
+app.get('/', async (req, res) => {
+    const { data: foundMatches } = await api.get<Search>('/');
+    const polishedMatches = polish(foundMatches);
+
+    res.setHeader('X-Total-Count', String(polishedMatches.bestMatches.length));
+
+    return res.json(polishedMatches.bestMatches);
+});
+
+app.get('/search/:symbol', async (req, res) => {
+    const stockName = req.params.symbol.toLowerCase();
+
+    const { data: foundMatches } = await api.get<Search>('/');
+    const polishedMatches = polish(foundMatches);
+
+    const stocks = polishedMatches.bestMatches.filter((stock) => {
+        const includesSymbol =
+            stock['symbol'].toLowerCase() === stockName.toLowerCase();
+        const includesName = stock['name']
+            .toLowerCase()
+            .includes(stockName.toLowerCase());
+
+        if (includesSymbol || includesName) return stock;
+    });
+
+    res.setHeader('X-Total-Count', String(stocks.length));
+
+    return res.json(stocks);
 });
 
 app.listen(3000);
