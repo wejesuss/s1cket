a9b2d440572fce4461a7c9c0f38621812648bd9c
Wemerson Jesus <wejesuss@gmail.com>
2020-07-17 16:15:59 -0300

    Add interfaces of the search and index route

---
+++ src/@types/Search.ts
@@ -0,0 +1,23 @@
+export type SearchResults = {
+    '1. symbol': string;
+    '2. name': string;
+    '3. type': string;
+    '4. region': string;
+    '5. marketOpen': string;
+    '6. marketClose': string;
+    '7. timezone': string;
+    '8. currency': string;
+    '9. matchScore': string;
+};
+
+export type PolishedSearchResults = {
+    symbol: string;
+    name: string;
+    type: string;
+    region: string;
+    marketOpen: string;
+    marketClose: string;
+    timezone: string;
+    currency: string;
+    matchScore: string;
+};
