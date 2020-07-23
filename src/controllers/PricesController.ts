94f0ac2b2080cb313562e76d6d27fac257a9fc52
Wemerson Jesus <wejesuss@gmail.com>
2020-07-23 18:49:42 -0300

    Validate route parameters

--- src/controllers/PricesController.ts
+++ src/controllers/PricesController.ts
@@ -1,48 +1,18 @@
 import { Response, Request } from 'express';
 import api from '../config/api';
 import { polish } from '../utils/util';
-import { Intradays } from '../@types/index';
+import {
+    PolishedIntradaysDailyAndWeekly,
+    IntradaysDailyAndWeekly,
+} from '../@types/index';
 import { AxiosResponse } from 'axios';
 
-const Prices = {
-    index: async (req: Request, res: Response): Promise<Response> => {
-        const stockName = req.params.symbol;
-        let { interval = '5min' } = req.query;
-        interval = String(interval).slice(0, 4);
-        let { outputsize = 'compact' } = req.query;
-        outputsize = String(outputsize);
-
-        const times = ['1min', '5min', '15min', '30min', '60min'];
-        if (!times.includes(interval)) {
-            return res.json({
-                error: 'interval param is not in the range covered by the api',
-            });
-        }
-
-        if (outputsize !== 'compact' && outputsize !== 'full') {
-            return res.json({
-                error: 'outputsize param should be "compact" or "full"',
-            });
-        }
-
-        const isNotInBrazil = stockName.slice(-3) !== 'SAO';
-        if (interval && outputsize && isNotInBrazil) {
-            const { data: intradayInfo } = await Prices.intraday(
-                stockName,
-                interval,
-                outputsize
-            );
-
-            // eslint-disable-next-line prettier/prettier
-            const polishedData = Prices.polishIntraday(intradayInfo);
-
-            return res.json(polishedData);
-        }
-
-        return res.json({ outputsize });
-    },
-    polishIntraday: (data: Intradays): Intradays => {
-        const result = polish<Intradays, Intradays>(data);
+const Helpers = {
+    polish: (data: IntradaysDailyAndWeekly) => {
+        const result = polish<
+            IntradaysDailyAndWeekly,
+            PolishedIntradaysDailyAndWeekly
+        >(data);
 
         return result;
     },
---
+++ src/validators/Schemes.ts
@@ -0,0 +1,51 @@
+import { Joi, Segments } from 'celebrate';
+
+const symbolSchema = {
+    [Segments.PARAMS]: Joi.object({
+        symbol: Joi.string().required().messages({
+            'string.empty': 'Symbol parameter can not be empty',
+            'any.required': "It's necessary a symbol as a parameter",
+        }),
+    }),
+};
+
+const searchSchema = {
+    [Segments.QUERY]: Joi.object({
+        search: Joi.string().max(1026).required().messages({
+            'string.base':
+                "Search parameter should be of type 'string'. You can separate values with ','",
+            'string.empty': 'Search parameter can not be empty',
+            'any.required': 'Please include search parameter',
+        }),
+    }),
+};
+
+const intradayAndDailyQueriesSchema = {
+    [Segments.QUERY]: {
+        interval: Joi.string()
+            .default('5min')
+            .regex(/^(1|5|15|30|60)min$/)
+            .messages({
+                'string.base': "interval parameter should be of type 'string'",
+                'string.empty': "'interval' is not allowed to be empty",
+                'string.pattern.base':
+                    "interval parameter is not in the range covered by the api: '1min' | '5min' | '15min' | '30min' | '60min'",
+            }),
+        outputsize: Joi.string()
+            .default('compact')
+            .regex(/^(compact|full)$/)
+            .messages({
+                'string.base':
+                    "outputsize parameter should be of type 'string'",
+                'string.empty': "'outputsize' is not allowed to be empty",
+                'string.pattern.base':
+                    "'outputsize' parameter should be 'compact' or 'full'",
+            }),
+    },
+};
+
+export default {
+    symbolSchema,
+    searchSchema,
+    intradayAndDailyQueriesSchema,
+};
