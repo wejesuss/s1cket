f73ebe6d05709436bc2dea70e5dc6f7a5eae3576
Wemerson Jesus <wejesuss@gmail.com>
2020-07-27 18:07:01 -0300

    Heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(routes);
 
 app.use(function (req, res) {
     res.status(404).json({ error: 'Try another route' });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Listening on PORT: ${port}`);
+});
