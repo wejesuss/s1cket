9533b11b683f00448b68775cd6456ff12640e516
Wemerson Jesus <wejesuss@gmail.com>
2020-08-26 10:37:21 -0300

    Polish currencies prices

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,5 @@
 import { PolishedSearch } from '../@types/index';
+import { physical } from '../validators/currencies.json';
 
 const keys = {
     'Error Message': 'error',
@@ -31,6 +32,9 @@ const keys = {
     'Time Series (60min)': 'timeSeries',
     'Time Series (Daily)': 'timeSeries',
     'Weekly Time Series': 'timeSeries',
+    'Time Series (Digital Currency Daily)': 'timeSeries',
+    'Time Series (Digital Currency Weekly)': 'timeSeries',
+    'Time Series (Digital Currency Monthly)': 'timeSeries',
 
     '1. open': 'open',
     '2. high': 'high',
@@ -60,9 +64,40 @@ const keys = {
     '7. Time Zone': 'timeZone',
     '8. Bid Price': 'bidPrice',
     '9. Ask Price': 'askPrice',
+
+    '2. Digital Currency Code': 'digitalCurrencyCode',
+    '3. Digital Currency Name': 'digitalCurrencyName',
+    '4. Market Code': 'marketCode',
+    '5. Market Name': 'marketName',
+    '6. market cap (USD)': 'marketCapUSD',
 };
 
 const timestamp = /[0-9]{4}-[0-9]{2}-[0-9]{2}( [0-9]{2}:[0-9]{2}:[0-9]{2})?/g;
+const currenciesDataKeys = {
+    '1a. open': 'open',
+    '1b. open': 'open',
+    '2a. high': 'high',
+    '2b. high': 'high',
+    '3a. low': 'low',
+    '3b. low': 'low',
+    '4a. close': 'close',
+    '4b. close': 'close',
+};
+
+const setCurrenciesDataKeys = (): void => {
+    const codes = physical.map((currency) => currency.code);
+    const keysValues = Object.entries(currenciesDataKeys);
+
+    codes.forEach((code) => {
+        keysValues.forEach((keyValue) => {
+            const [key, value] = keyValue;
+            const dataKey = key + ` (${code})`;
+            const dataKeyFormatted = value + `${code}`;
+
+            keys[dataKey] = dataKeyFormatted;
+        });
+    });
+};
 
 const polish = <T = unknown, R = PolishedSearch>(data: T): R => {
     if (!data || typeof data !== 'object') return (data as unknown) as R;
@@ -97,4 +132,4 @@ const splitComma = (value: unknown): Array<string> =>
         .map((value) => value.trim())
         .filter((value) => value);
 
-export { polish, splitComma };
+export { polish, splitComma, setCurrenciesDataKeys };
